f893ae5a1d3ac6063c903c085a4fbb58cfc06a36
Usuario@DESKTOP-UK4QPB5 <gabrielsiqueiradealmeida@gmail.com>
2024-09-05 23:47:24 -0300

    usando @Query para consultas JPQL

--- SGT/src/main/java/br/edu/iff/ccc/bsi/webdev/repository/TaskRepository.java
+++ SGT/src/main/java/br/edu/iff/ccc/bsi/webdev/repository/TaskRepository.java
@@ -1,12 +1,24 @@
 package br.edu.iff.ccc.bsi.webdev.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
 
 import br.edu.iff.ccc.bsi.webdev.entities.Tarefa;
 
 
 @Repository
 public interface TaskRepository extends JpaRepository<Tarefa, Long>{
+
+  @Query("SELECT t FROM Tarefa t WHERE t.status = :status")
+List<Tarefa> findByStatus(@Param("status") String status);
+
+@Query("SELECT t FROM Tarefa t WHERE t.dataCriacao > :data")
+List<Tarefa> findTasksCreatedAfter(@Param("data") LocalDate data);
+
 
 }
