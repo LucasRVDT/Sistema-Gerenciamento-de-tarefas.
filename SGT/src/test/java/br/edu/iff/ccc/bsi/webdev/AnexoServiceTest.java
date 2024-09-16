115662a76a6b3e0da7f22f6d4297d4e0d80d7fcd
GSiqueiraA <90071227+GSiqueiraA@users.noreply.github.com>
2024-09-16 07:59:14 -0300

    Create AnexoServiceTest.java

---
+++ SGT/src/test/java/br/edu/iff/ccc/bsi/webdev/AnexoServiceTest.java
@@ -0,0 +1,62 @@
+package br.edu.iff.ccc.bsi.webdev.entities;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Size;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+@Table(name = "tb_item")
+public abstract class Item implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotNull(message = "O campo nome não pode ser nulo.")
+    @Column(name = "nome", nullable = false)
+    private String nome;
+
+    @Size(min = 5, max = 200, message = "A descricao deve ter entre 5 e 200 caracteres.")
+    @Column(name = "descricao")
+    private String descricao;
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
