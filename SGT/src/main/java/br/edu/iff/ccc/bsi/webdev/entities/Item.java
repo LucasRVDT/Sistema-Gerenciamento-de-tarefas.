2938e1c0d047cc931ff28e1208c91a7c94988413
GSiqueiraA <90071227+GSiqueiraA@users.noreply.github.com>
2024-09-16 08:33:00 -0300

    Update Item.java

--- SGT/src/main/java/br/edu/iff/ccc/bsi/webdev/entities/Item.java
+++ SGT/src/main/java/br/edu/iff/ccc/bsi/webdev/entities/Item.java
@@ -13,7 +13,6 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.Size;
 
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 
 
 @Entity
