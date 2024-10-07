9a2e01256355c0e1fe0f2074f1a9b03c7d2784ae
GSiqueiraA <90071227+GSiqueiraA@users.noreply.github.com>
2024-10-07 19:00:03 -0300

    Update UserNotFoundException.java

--- SGT/src/main/java/br/edu/iff/ccc/bsi/webdev/exceptions/UserNotFoundException.java
+++ SGT/src/main/java/br/edu/iff/ccc/bsi/webdev/exceptions/UserNotFoundException.java
@@ -3,12 +3,26 @@ package br.edu.iff.ccc.bsi.webdev.exceptions;
 @SuppressWarnings("serial")
 public class UserNotFoundException extends RuntimeException {
 
-	public final String url;
-	public final String ex;
-	
-	public UserNotFoundException(String url, Exception ex) {
-		this.url = url;
-		this.ex = ex.getLocalizedMessage();
-	}
-	
+    private final String url;
+    private final String ex;
+
+    public UserNotFoundException(String url, Exception ex) {
+        super(ex);  
+        this.url = url;
+        this.ex = ex.getLocalizedMessage();
+    }
+
+    public UserNotFoundException(String url, String message) {
+        super(message); 
+        this.url = url;
+        this.ex = message;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getExceptionMessage() {
+        return ex;
+    }
 }
